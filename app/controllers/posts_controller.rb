d72c2753b8a58c728baa12c9b686a5b0e4e22748
paul <ajuzepaul>
2020-07-02 17:44:37 +0300

    remove modification and delete links from view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[show edit update destroy]
   before_action :authenticate_user!, except: %i[index show]
 
   # GET /posts
@@ -28,7 +27,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to posts_url, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
